6df649e004171ddce411f63c3b100eff8f109a65
The CNQRR <ap166884@gmail.com>
2026-08-26 19:26:33 +0700

    Расширенные операции

--- src/main/java/service/ChatService.java
+++ src/main/java/service/ChatService.java
@@ -3,6 +3,8 @@ package service;
 import interfaces.Content;
 import model.Message;
 
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -72,6 +74,13 @@ public final class ChatService {
         return snapshot.stream().sorted(Comparator.comparing(Message::sentAt)).skip(n).toList();
     }
 
+    public Map<LocalDate, Long> countByDay(long chatId) {
+        List<Message> snapshot = getMessagesByChatId(chatId);
+
+        return snapshot.stream().collect(Collectors.groupingBy(
+                m -> m.sentAt().atZone(ZoneOffset.UTC).toLocalDate(), Collectors.counting()));
+    }
+
     public Optional<Long> mostActiveAuthor(long chatId) {
         List<Message> snapshot = getMessagesByChatId(chatId);
 
@@ -80,5 +89,71 @@ public final class ChatService {
         return countByAuthor.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey);
     }
 
+    public Set<String> allWords(long chatId) {
+        List<Message> snapshot = getMessagesByChatId(chatId);
 
+        List<Message> textMessages = snapshot.stream().filter(message -> message.content() instanceof Content.TextContent).toList();
+
+        return textMessages.stream()
+                .map(message ->  ((Content.TextContent) message.content()).text().split(" "))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toSet());
+    }
+
+    public Map<Long, Long> topChatsByActivity(int n) {
+        HashMap<Long, List<Message>> snapshot = getMessages();
+
+        Map<Long, Long> messagesCount = snapshot.values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.groupingBy(Message::chatId, Collectors.counting()));
+
+
+        return messagesCount.entrySet().stream().sorted(Map.Entry.<Long, Long>comparingByValue().reversed())
+                .limit(n)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (a, b) -> a,
+                        LinkedHashMap::new
+                ));
+    }
+
+    public long totalFileSize() {
+        HashMap<Long, List<Message>> snapshot = getMessages();
+
+        return snapshot.values().stream().flatMap(Collection::stream)
+                .filter(message -> message.content() instanceof Content.FileContent)
+                .mapToLong(message -> ((Content.FileContent) message.content()).sizeBytes())
+                .sum();
+    }
+
+    public Map<String, Long> countByContentType() {
+        HashMap<Long, List<Message>> snapshot = getMessages();
+
+         return snapshot.values().stream().flatMap(Collection::stream)
+                 .collect(Collectors.groupingBy(m -> switch (m.content()) {
+                     case Content.TextContent _ -> "TextContent";
+                     case Content.ImageContent _ -> "ImageContent";
+                     case Content.FileContent _ -> "FileContent";
+                 }, Collectors.counting()));
+    }
+
+    public Map<Boolean, List<Message>> splitByImage() {
+        HashMap<Long, List<Message>> snapshot = getMessages();
+
+        return snapshot.values().stream().flatMap(Collection::stream)
+                .collect(Collectors.partitioningBy(
+                        message -> message.content() instanceof Content.ImageContent));
+    }
+
+    public Map<Long, Message> lastMessagePerChat() {
+        HashMap<Long, List<Message>> snapshot = getMessages();
+
+        return snapshot.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry ->
+                                entry.getValue().stream().max(Comparator.comparing(Message::sentAt)).orElseThrow()
+                ));
+    }
 }
